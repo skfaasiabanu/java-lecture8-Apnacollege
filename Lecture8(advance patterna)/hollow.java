cbb42a190ca63584c1d43fbeddc7f0602ceab05e
SHAIK FAASIA BANU <skfaasiabanu178@gmail.com>
2024-11-14 21:18:14 +0530

    lecture 8

---
+++ Lecture8(advance patterna)/hollow.java
@@ -0,0 +1,25 @@
+public class hollow
+{
+    public static void  rectangle(int rows,int cols)
+
+    {
+        for(int i=1;i<=rows;i++)
+        {
+            for(int j=1;j<=cols;j++)
+            {
+                if(i==1||i==rows ||j==1||j==cols)
+                {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" "); 
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[])
+    {
+         rectangle(4,5);
+    }
+}
---
+++ Lecture8(advance patterna)/zeroonetriangle.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class zeroonetriangle
+{
+    public static void inver(int n)
+    {
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                if((i+j)%2==0)
+                {
+                    System.out.print(1);
+                }
+                else
+                {
+                    System.out.print(0);
+                }
+            }
+            
+            System.out.println();
+        }
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        inver(n);
+    }
+}
